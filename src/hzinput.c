5eeb9cb877fdf94bc4b1931d3c43d9f05231e838
descent <descent0@gmail.com>
2012-10-20 22:25:49 +0800

    modify VERSION_STRING string

--- src/hzinput.c
+++ src/hzinput.c
@@ -6,7 +6,7 @@
 /****************************************************************************/
 
 //#define POS_OF_VERSIONSTRING 0
-#define VERSION_STRING "descent modify 1.4 rc2 "
+#define VERSION_STRING "descent modify jmcce 1.4 rc2 80x24"
 //#define INPUT_FGCOLOR 0
 //#define INPUT_BGCOLOR 0
 
